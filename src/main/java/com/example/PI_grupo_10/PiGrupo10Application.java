fdcf9163fa41b4c8c974b94111a72714dac6ac8e
javtr <92610538+javtr@users.noreply.github.com>
2022-11-25 20:35:14 -0500

    Update PiGrupo10Application.java

--- src/main/java/com/example/PI_grupo_10/PiGrupo10Application.java
+++ src/main/java/com/example/PI_grupo_10/PiGrupo10Application.java
@@ -56,7 +56,7 @@ public class PiGrupo10Application {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowedOriginPatterns(Arrays.asList(""));
-		config.setAllowedOrigins(Arrays.asList("http://localhost:3000/", "*"));
+		//config.setAllowedOrigins(Arrays.asList("http://localhost:3000/", "*"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowCredentials(true);
 		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
